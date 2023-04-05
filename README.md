25224a96889af6740a46f96754da8c3a75f37aa5
Iago leal Do Nascimento <iago.l.nascimento@gmail.com>
2023-04-04 23:12:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Html-css
+ Curso em Vídeo
---
+++ exercicios/ex0017/teste.js
@@ -0,0 +1,11 @@
+const petName="Jessie"
+const breed="Labrador"
+
+console.log (petName);
+console.log (Labrador);
+
+window.alert ('hello World')
+
+
+
+
